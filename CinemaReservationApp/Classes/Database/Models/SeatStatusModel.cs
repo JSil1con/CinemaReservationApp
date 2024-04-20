9281a5fb6a6cf4888b25e15dd2962ee21476f21d
JSil1con <daniel.kremen@email.cz>
2024-04-20 20:39:52 +0200

    added seat status model

---
+++ CinemaReservationApp/Classes/Database/Models/SeatStatusModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaReservationApp.Classes.Database.Models
+{
+    internal class SeatStatusModel
+    {
+        public int Id { get; set; }
+        public string Status { get; set; }
+    }
+}
