ea8eb59c2bc4c3da4978fba20ec9d7ad5c8552a7
JSil1con <daniel.kremen@email.cz>
2024-04-20 22:49:06 +0200

    View cinemas from database

---
+++ CinemaReservationApp/Views/SelectCinema.xaml.cs
@@ -0,0 +1,51 @@
+﻿using CinemaReservationApp.Classes.Database;
+using CinemaReservationApp.Classes.Database.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CinemaReservationApp.Views
+{
+    /// <summary>
+    /// Interakční logika pro SelectCinema.xaml
+    /// </summary>
+    public partial class SelectCinema : Window
+    {
+        private Database _database = new Database("database.db");
+        public SelectCinema()
+        {
+            InitializeComponent();
+
+            ViewCinemasAsync();
+        }
+
+        private async void ViewCinemasAsync()
+        {
+            List<CinemaModel> cinemas = await _database.GetCinemasAsync();
+
+            int row = 0;
+
+            foreach (var cinema in cinemas)
+            {
+                MainGrid.RowDefinitions.Add(new RowDefinition());
+                Button cinemaButton = new Button();
+                cinemaButton.Content = cinema.Name;
+                Grid.SetRow(cinemaButton, row);
+                Grid.SetColumn(cinemaButton, 0);
+                MainGrid.Children.Add(cinemaButton);
+                row++;
+            }
+        }
+
+    }
+}
