dfde5cefb24cb25a1727e6daebf53a2f1752b6d2
JSil1con <daniel.kremen@email.cz>
2024-04-20 20:39:05 +0200

    Added seatmodel

---
+++ CinemaReservationApp/Classes/Database/Models/SeatModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaReservationApp.Classes.Database.Models
+{
+    internal class SeatModel
+    {
+        public int Id { get; set; }
+        public int Row { get; set; }
+        public int Column { get; set; }
+        public int CinemaId { get; set; }
+        public int SeatStatusId { get; set; }
+    }
+}
